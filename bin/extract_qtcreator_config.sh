1d5e74079466edbc14e4ead4be56a6e1916c7f3a
Dmitry Kazakov <dimula73@gmail.com>
2021-09-29 14:09:23 +0300

    Add a script for saving QtCreator config to the host system

---
+++ bin/extract_qtcreator_config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source ${DIR}/find_default_container_file.inc
+
+container_name=$(parseContainerArgs $*)
+if [ -z ${container_name} ]; then
+    exit 1
+fi
+
+tryStartContainer $container_name
+
+${DOCKER_BINARY} cp ${container_name}:/home/appimage/.config/QtProject/QtCreator.ini ~/.config/QtProject/QtCreator.ini
