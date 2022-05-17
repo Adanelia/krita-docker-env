caa6bf745854b9c66bd871c4c9d48e62c8d50b42
Dmitry Kazakov <dimula73@gmail.com>
2022-05-17 09:26:47 +0300

    Activate auto-compression when downloading Krita deps
    Thanks to Nicolas jenkins now supports auto-compressing the build
    artifacts when requested. That will save a lot of time for developers.
    
    The default build of wget in Ubuntu 18.04 doesn't support compression,
    so I added a fallback to curl.
    
    See this SO answer:
    https://unix.stackexchange.com/questions/464372/how-could-compression-be-missing-from-my-wget

--- bin/bootstrap-deps.sh
+++ bin/bootstrap-deps.sh
@@ -7,7 +7,12 @@ fi
 if [ ! -f ./persistent/krita-appimage-deps.tar ]; then
     (
         cd ./persistent/
-        wget https://binary-factory.kde.org/job/Krita_Nightly_Appimage_Dependency_Build/lastSuccessfulBuild/artifact/krita-appimage-deps.tar || exit 1
+
+        if wget --version | grep \\-lz > /dev/null; then
+            wget --compression=auto https://binary-factory.kde.org/job/Krita_Nightly_Appimage_Dependency_Build/lastSuccessfulBuild/artifact/krita-appimage-deps.tar || exit 1
+        else
+            curl -LO --compressed https://binary-factory.kde.org/job/Krita_Nightly_Appimage_Dependency_Build/lastSuccessfulBuild/artifact/krita-appimage-deps.tar || exit 1
+        fi
     )
 fi
 
